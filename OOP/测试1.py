81c9523fc33421aa67c97bf9224ed1e79ca7ad51
feimaoweibo <531148362@qq.com>
2019-01-22 18:00:04 +0800

    新增内置的数学模块与随机数模块的练习习题

--- OOP/测试1.py
+++ OOP/测试1.py
@@ -57,18 +57,70 @@ list1 =  [34,890,54,1,7777777]
 print(list1)
 random.shuffle(list1)
 print(list1)
-'''
+
 
 #作业题
 #输入三位数，并于系统给出随机数字比较大小，大于给出的随机数，打印今晚可以买彩票了，小于打印你猜错了。
 import random
+import math
 num = input("请输入三位数字")
-for i in range(1,2):
+random_sjs = random.randrange(100,1000)
+# print(random_sjs)
+# print(type(random_sjs))
+if num.isdigit() and 100 <= int(num) <= 999:
+    if int(num) > random_sjs :
+        print("比随机数大了")
+    if int(num) == random_sjs:
+        print("恭喜你猜中了")
+    if int(num) < random_sjs :
+        print("比随机数小了")
+'''
 
-    list1 = print(random.randrange(100,999))
-int(i) = list1
-if int(num) > int(i):
-    print("今晚可以买彩票了")
+'''
+输入一个三位数与程序随机数比较大小
+1、如果大于程序随机数，则分别输出这个三位数的个、十、百的数字；
+2、如果等于程序随机数，则提示恭喜中奖了；
+3、如果小于程序随机数，则打印120个字符，规则如下：
+    a、每行字符串长度为12；
+    b、前四个字符为字母；
+    c、后八个字符为数字
+'''
+import random
+import math
+
+num = input("请输入一个三位数： ")
+# 定义一个变量，把随机数赋值给它，作为程序随机数
+random_sjs = random.randrange(100,1000)
+if num.isdigit() and 100 <= int(num) <= 999: # .insdigit()函数用来判断输入内容是否是数字的先决条件
+    if int(num) == random_sjs: # int()函数把输入数字用作为数字类型
+        print("恭喜你，千分之一的中奖概率都给你碰上了")
+    if int(num) > random_sjs:
+        bai = int(num)//100    # “//”符号，表示整除，返回商的整数部分，即得到百位数上的数字
+        shi = int(num)%100//10 # “%”符号，表示取模，返回除法的余数；比如num=333,num%100 ==33,表示先把333先除100得到余数33 ->再把33//10 ==3,表示把33整除10得到3为返回商的整数，即得到十位数上的数字
+        ge  = int(num)%100%10  #  比如数字987,解析步骤：987%100==87，余数为87  -> 87%10==7,表示余数为7，即得到个位上的数字
+        print("个位数是：{0}, 十位数是：{1},百位数是：{2}".format(ge,shi,bai))
+    if int(num) < random_sjs:
+        for l in range(11): # 根据要求打印120个字符，每行12个字符，需要打印10行
+            str_t1 = '' # 定义一个变量，进行4次拼接动作存放字母字符，
+            for i in range(4):
+                str_name = random.randrange(65,123) # 定义变量用来存放65到122之间其中的一个数字
+                str_ascii = chr(str_name) # 用chr()函数把acsii编码在65与122之间的数字，转换成52字母，包含大小写字母
+                str_t1 =str_t1 + str_ascii # 拼接4次循环产生的字母
+                # print(str_t1) 打印出每次转换后的字母
+            # print(str_t1) 外层打印最后拼接4的字母
+            str_t2 = ''
+            for j in range(9):
+                str_num = random.randrange(1,10)
+                str_strnum = str(str_num)
+                str_t2 =str_t2 + str_strnum
+                # print(str_t2) 打印出每次循环随机生成的数字
+            # print(str_t1) 外层打印最后一次循环生成的数字
+
+            str_row = str_t1 + str_t2 # 拼接字母与数字
+
+            print(str_row) # 打印10次生成的字母与数字组成的字符串
+
+
+
 else:
-    print("你猜错了")
-
+    print('请重新输入一个三位数')
