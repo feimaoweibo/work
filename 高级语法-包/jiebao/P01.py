0c9276f908c6d6f7f65689452a6a89d2c6a93d68
feimaoweibo <531148362@qq.com>
2019-05-07 17:27:27 +0800

    编程练习-新增练习用文件夹

---
+++ 高级语法-包/jiebao/P01.py
@@ -0,0 +1,15 @@
+'''
+- 包含一个学生类
+- 一个sayhello函数
+- 一个打印语句
+'''
+class Student():
+    def __init__(self,name='noname',age=18):
+        self.name = name
+        self.age = age
+    def say(self):
+        print('My name is {0}'.format(self.name))
+def sayHello():
+    print('Hi,欢迎学习Python')
+if __name__ == '__main__':
+    print('我是第一个模块P01')
