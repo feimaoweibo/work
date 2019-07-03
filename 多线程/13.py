e752473ec80f2f5259e910c8b1c9a2f59d23ea68
feimaoweibo <531148362@qq.com>
2019-07-03 17:42:17 +0800

    编程练习-多线程-案例3-14

--- 多线程/13.py
+++ 多线程/13.py
@@ -14,4 +14,24 @@ class Producer(threading.Thread):
                     # put 是往queue中放入一个值
                     queue.put(msg)
                     print(msg)
-            time.sleep(0.5)
+            time.sleep(20)
+class Consumer(threading.Thread):
+    def run(self):
+        global queue
+        while True:
+            if queue.qsize() > 100:
+                for i in range(3):
+                    # get是从queue中取出一个值
+                    msg = self.name + "消费了"+queue.get()
+                    print(msg)
+                time.sleep(1)
+if __name__ == "__main__":
+    queue = queue.Queue()
+    for i in range(500):
+        queue.put('初始产品'+str(i))
+    for i in range(2):
+        p = Producer()
+        p.start()
+    for i in range(5):
+        c = Consumer()
+        c.start()
