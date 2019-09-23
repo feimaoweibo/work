959e042ea95cc3cce1c32d6650591aa8564e270e
feimaoweibo <531148362@qq.com>
2019-09-23 17:21:00 +0800

    编程练习-Django框架-概述-视图Request

--- Django/tulingxueyuan_views/teacher_app/views.py
+++ Django/tulingxueyuan_views/teacher_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-
+from django.core.urlresolvers import reverse
 # Create your views here.
 
 def teacher(req):
@@ -9,3 +9,28 @@ def teacher(req):
 def v2_exception(r):
     raise Http404
     return HttpResponse("OK")
+
+def v10_1(request):
+    return HttpResponseRedirect('/v11')
+def v10_2(request):
+    return HttpResponseRedirect(reverse("v11")),
+def v11(request):
+    return HttpResponse("哈哈，这是v11的访问返回内容")
+
+def v8_get(request):
+    rst = ""
+    for k,v in request.GET.items():
+        # 采用键值对字典格式
+        rst = rst + k + "--->" + v
+        rst += ","
+    return HttpResponse("Get value of Request is {0}".format(rst))
+
+def v9_get(requset):
+    # 渲染模版并返回
+    return render_to_response('for_post.html')
+def v9_post(request):
+    rst = ''
+    for k,v in request.POST.items():
+        rst += k + "---->" + v
+        rst += "，"
+    return HttpResponse("Post value of POST is {0}".format(rst))
--- Django/tulingxueyuan_views/tulingxueyuan_views/urls.py
+++ Django/tulingxueyuan_views/tulingxueyuan_views/urls.py
@@ -2,14 +2,26 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from teacher_app import views as v
-urlpatterns = [
+urlpatterns = {
     # Examples:
     # url(r'^$', 'tulingxueyuan_views.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^teacher/', v.teacher),
-    url(r'^v2_exp/', v.v2_exception),
+    url(r'^teacher/', v.teacher),  # 2、简单视图
+    url(r'^v2_exp/', v.v2_exception),  # http404异常案例
 
-]
+    # 4、HttpResponseRedirect重定向
+    url(r'^v10_1/', v.v10_1),
+    url(r'^v10_2/', v.v10_2),
+    url(r'^v11/', v.v11, name="v11"),
+
+    # 5、Request对象get()方法案例
+    url(r'^v8/', v.v8_get),
+
+
+    # 5、Request对象post()方法案例
+    url(r'^v9_get', v.v9_get),
+    url(r'^v9_post', v.v9_post),
+}
