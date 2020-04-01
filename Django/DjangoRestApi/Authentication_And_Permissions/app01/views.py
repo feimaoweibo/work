d92267de2a74705d46b2f588859177241a5a46a9
feimaoweibo <531148362@qq.com>
2020-04-01 22:03:46 +0800

    Django-REST-Framework Authentication_And_Permissions

---
+++ Django/DjangoRestApi/Authentication_And_Permissions/app01/views.py
@@ -0,0 +1,97 @@
+from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from app01 import models
+from app01 import serializers
+from rest_framework import mixins
+from rest_framework import generics
+from rest_framework import permissions
+from app01.permissions import IsOwnerOrReadOnly
+# Create your views here.
+
+# 列出所有的出版社或者创建一个新的出版社操作
+'''
+class PublisherList(APIView):
+    # 列出所有的出版社或者创建一个新的出版社操作
+    def get(self, request, format=None):
+        queryset = models.Publisher.objects.all() # 查询出所有的出版社
+        s = serializers.PublisherSerializer(queryset, many=True)
+        return Response(s.data)
+    def post(self, request, format=None):
+        s = serializers.PublisherSerializer(data=request.data)
+        if s.is_valid(): # 如果传入数据没问题
+            s.save()
+            return Response(s.data, status=status.HTTP_201_CREATED)
+        return Response(s.errors, status=status.HTTP_400_BAD_REQUEST) #数据有问题则返回errors
+'''
+'''
+class PublisherList(mixins.ListModelMixin,
+                    mixins.CreateModelMixin,
+                    generics.GenericAPIView):
+    queryset = models.Publisher.objects.all()
+    serializer_class = serializers.PublisherSerializer
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+'''
+class PublisherList(generics.ListCreateAPIView):
+    queryset = models.Publisher.objects.all()
+    serializer_class = serializers.PublisherSerializer
+    # 权限设置，未元组格式内容。如果值为空，则所有人都能查看；permissions.IsAuthenticatedOrReadOnly表示登陆者均可以修改；
+    # IsOwnerOrReadOnly表示只有录入者才能修改自己录入的内容
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+    # 为登陆用户创建可以POST创建数据的权利
+    def perform_create(self, serializer):
+        serializer.save(operator=self.request.user)
+
+
+# 具体到一个出版社的数据，进行查看、修改、删除操作
+'''
+class PublisherDetail(APIView):
+    def get_object(self, pk): #通过输入的主键，先查询存在具体的出版社与否
+        try:
+            return models.Publisher.objects.get(pk=pk)
+        except models.Publisher.DoesNotExist:
+            raise Http404
+    # 查看具体的出版社信息
+    def get(self, request, pk, format=None):
+        publisher = self.get_object(pk)
+        s = serializers.PublisherSerializer(publisher)
+        return Response(s.data)
+    # 修改出版社信息
+    def put(self, request, pk, format=None):
+        publisher = self.get_object(pk)
+        s = serializers.PublisherSerializer(publisher, data=request.data)
+        if s.is_valid():
+            s.save()
+            return Response(s.data)
+        return Response(s.errors, status=status.HTTP_400_BAD_REQUEST)
+    # 删除出版社信息
+    def delete(self, request, pk, format=None):
+        publisher = self.get_object(pk)
+        publisher.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+'''
+'''
+class PublisherDetail(mixins.RetrieveModelMixin,
+                      mixins.UpdateModelMixin,
+                      mixins.DestroyModelMixin,
+                      generics.GenericAPIView):
+    queryset = models.Publisher.objects.all()
+    serializer_class = serializers.PublisherSerializer
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+'''
+class PublisherDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Publisher.objects.all()
+    serializer_class = serializers.PublisherSerializer
+    # 权限设置，未元组格式内容。如果值为空，则所有人都能查看；permissions.IsAuthenticatedOrReadOnly表示登陆者均可以修改；
+    # IsOwnerOrReadOnly表示只有录入者才能修改自己录入的内容
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
